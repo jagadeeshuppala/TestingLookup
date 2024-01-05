8627ff0b8af624f9e4f9220917b2673009640722
jagadeesh uppala <uppala.jagadeesh@gmail.com>
2024-01-05 16:26:15 +0000

    from pharmacy

--- src/main/java/org/example/TridentOrderProcess.java
+++ src/main/java/org/example/TridentOrderProcess.java
@@ -6,7 +6,9 @@ public class TridentOrderProcess {
 
     public static void main(String[] args) throws Exception {
 
-        String fileName = "C:\\PharmacyProjectWorkspace\\TestingLookup\\src\\main\\resources\\newSpreadSheet_copy_4_1_2024.xlsx";
+        //String fileName = "C:\\PharmacyProjectWorkspace\\TestingLookup\\src\\main\\resources\\newSpreadSheet_copy_4_1_2024.xlsx";
+        String fileName = "\\\\11701279QSVR\\PSSharedarea\\Bridgwater\\Miscellaneous\\OrderList.xlsx";
+
         int tridentResultsColNumber = 17;
         int quantityColNumber = 3;
         TridentOrderToBePlaced tridentOrderToBePlaced = new TridentOrderToBePlaced(fileName, tridentResultsColNumber, quantityColNumber);
